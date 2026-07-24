62c57e6d8b981329be0fe6daea1da52b29eec845
damovie347-wq <damovie347@gmail.com>
2026-07-24 22:20:21 +0300

    Add files via upload

--- app/src/main/java/com/usbmanager/app/App.kt
+++ app/src/main/java/com/usbmanager/app/App.kt
@@ -1,1 +1,16 @@
+package com.usbmanager.app
 
+import android.app.Application
+import com.usbmanager.app.theme.ThemeManager
+
+/**
+ * Uygulama sinifi. Tek gorevi: kullanicinin en son sectigi temayi
+ * (Acik / Karanlik / AMOLED / Sistem) ilk Activity olusmadan ONCE
+ * uygulamaktir; boylece acilista tema "cakmasi/yanip sonme" olmaz.
+ */
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        ThemeManager.applyNightMode(ThemeManager.currentMode(this))
+    }
+}
